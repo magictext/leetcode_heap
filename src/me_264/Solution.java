3b05069622947ed603e233abc10365489d4a88dc
magictext <743145068@qq.com>
2018-10-21 17:02:43 +0800

    好难...

---
+++ src/me_264/Solution.java
@@ -0,0 +1,35 @@
+package me_264;
+
+import java.util.HashSet;
+
+public class Solution {
+	public int nthUglyNumber(int n) {
+       HashSet<Integer> hashSet=new HashSet<>();
+       hashSet.add(1);
+       int count=1,x=0;
+       while (count!=n) {
+    	   x++;
+    	   if(x%2==0&&hashSet.contains(x/2)){
+    		   hashSet.add(x);
+    		   System.out.println(x);
+    		   count++;
+    	   }
+    	   else if(x%3==0&&hashSet.contains(x/3)){
+    		   System.out.println(x);
+    		   hashSet.add(x);
+    		   count++;
+    		   //continue;
+    	   }
+    	   else if(x%5==0&&hashSet.contains(x/5)){
+    		   System.out.println(x);
+    		   hashSet.add(x);
+    		   count++;
+    		   //continue;
+    	   }
+       }
+       return x;
+    }
+	public static void main(String[] args) {
+		System.out.println(new Solution().nthUglyNumber(169));
+	}
+}
---
+++ src/me_264/Solution2.java
@@ -0,0 +1,57 @@
+package me_264;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.TreeSet;
+
+public class Solution2 {
+	public int nthUglyNumber(int n) {
+		int temp = 0;
+		TreeSet<Integer> treeSet = new TreeSet<>();
+		TreeSet<Integer> tempset1 = new TreeSet<>();
+		TreeSet<Integer> tempset2 = new TreeSet<>();
+		tempset1.add(1);
+		tempset2.add(1);
+		treeSet.add(1);
+		int flag = 0;
+		int min=0;
+		while (n >= min) {
+			for (Integer integer : tempset1) {
+				if (flag == 0) {
+					flag = 1;
+					if (temp > integer * 2) {
+						tempset2.add(integer * 2);
+						// tempset2.remove(integer);
+						flag = 0;
+					}
+					if (temp > integer * 3) {
+						tempset2.add(integer * 3);
+						// tempset2.remove(integer);
+						flag = 0;
+					}
+					if (flag == 1) {
+						min=integer;
+						tempset2.add(integer * 5);
+						// tempset2.remove(integer);
+						flag = 0;
+					}
+				}
+
+			}
+
+			treeSet.addAll(tempset2);
+			Object obj[] = treeSet.toArray();
+			temp = (Integer) obj[obj.length - 1];
+			tempset1 = (TreeSet<Integer>) tempset2.clone();
+			// tempset2.clear();
+		}
+		ArrayList<Integer> arrayList = new ArrayList<>(treeSet);
+		// System.out.println(arrayList);
+		System.out.println(arrayList);
+		return arrayList.get(n - 1);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new Solution2().nthUglyNumber(18));
+	}
+}
